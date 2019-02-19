e624b6004fbdefbad22dbc38faec8d2e4f84d953
deepumoses <sonicdeepak@gmail.com>
2019-02-19 20:27:37 +0530

    Add files via upload

---
+++ js/effects.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	/*alert("hi");*/
+	$(".logo .logoimg").css({"width":"350px",
+							 "height":"100px"});
+	$(".love").css("opacity","1");
+	$(".friendship").css("opacity","1");
+	$(".funny").css("opacity","1");
+	$(".sad").css("opacity","1");
+	$(".cute").css("opacity","1");
+	$(".cool").css("opacity","1");
+	$(".motivational").css("opacity","1");
+	$(".sarcastic").css("opacity","1");
+	$(".religious").css("opacity","1");
+});
